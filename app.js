602db573c4c8b5d4c703663e86e4a77c011c4640
Bhargav Patel <bhargav.patel@bacancytechnology.com>
2018-10-18 18:30:43 +0530

    tutorial 10

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ fun1(fun2);*/
 // console.log(utils.getString(['Hiren', 'Bhargav', 'Ashvin']));
 // console.log(utils.counter(['Hiren', 'Bhargav', 'Ashvin']));
 
+
+
 //TODO Simple Emit Demo
 // var events = require('events');
 // var myEmitter = new events.EventEmitter();
@@ -32,26 +34,40 @@ fun1(fun2);*/
 // myEmitter.emit('test1',"test 1");
 // myEmitter.emit('test2',"test 2");
 
+
+
 //TODO Multiple Type and obj Demo
-var events = require('events');
-var util = require('util');
-var Person = function(name){
-    this.name = name ;
-}
-util.inherits(Person, events.EventEmitter);
-var hiren = new Person('Hiren')
-var bhargav = new Person('Bhargav')
-var Ashvin = new Person('Ashvin')
-var peopleArr = [ hiren, bhargav, Ashvin ];
-peopleArr.forEach(function(person){
-    person.on('speak',function(msg){
-        console.log(person.name+' says: '+msg);
-    })
-})
+// var events = require('events');
+// var util = require('util');
+// var Person = function(name){
+//     this.name = name ;
+// }
+// util.inherits(Person, events.EventEmitter);
+// var hiren = new Person('Hiren')
+// var bhargav = new Person('Bhargav')
+// var Ashvin = new Person('Ashvin')
+// var peopleArr = [ hiren, bhargav, Ashvin ];
+// peopleArr.forEach(function(person){
+//     person.on('speak',function(msg){
+//         console.log(person.name+' says: '+msg);
+//     })
+// })
+// hiren.emit('speak',"Hi Good morning");
+// bhargav.emit('speak',"Hi very Good morning");
+// Ashvin.emit('speak',"Hi :)");
 
+var fs = require('fs');
+// var readme = fs.readFileSync('README.md', 'utf8'); // For load file in var
+// fs.writeFileSync('HIREN VAGHELA',readme) // Create and paste file
 
-hiren.emit('speak',"Hi Good morning");
-bhargav.emit('speak',"Hi very Good morning");
-Ashvin.emit('speak',"Hi :)");
+// fs.readFile('README.md','utf8', function(err,data){
+//     // console.log(data)
+//     fs.writeFile('WriteMe.txt', data); // Create and paste file
+// })
 
+// fs.readFile('WriteMe.txt','utf8', function(err,data){ 
+//     console.log(data)    
+// })
+fs.unlink('WriteMe.txt'); // DELETE file
+fs.mkdirSync('Test2');
 
